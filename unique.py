eacb1b321f8ecb026d632bf8a504fa1ef5d68106
Madhav Ghei <madhav.ghei@gmail.com>
2018-08-05 13:55:05 -0500

    updated references_license.csv

--- unique.py
+++ unique.py
@@ -1,36 +1,28 @@
-# import os
-
-# root_dir = './references_revised/'
-
-# dirnames = [] # collect all category directories in this list
-# fnList = [] # collect all filenames in this list
-# output = [] # collect all UNIQUE filenames in this list
-
-# for refdirname, refdirnames, reffilenames in os.walk(root_dir):
-# 	dirnames.append(refdirname)
-
-# dirnames = dirnames[1:] # remove root directory from directory list, only include subfolders (which are the categories)
-
-
-# for dr in dirnames:
-# 	for rdname, rdnames, fns in os.walk(dr):
-# 		fnList.append(fns) # add all lists of filenames per category to the list
-
-# fnList = [item for sublist in fnList for item in sublist] # flatten the list so we just have a list of all audio filenames
-
-# print len(fnList)
-
-# for x in fnList:
-#     if x not in output:
-#         output.append(x) # make a copy of the filename list containing only unique filenames
-
-# print len(output) # number of unique named audio files
-
 import os
-cpt = (files for r, d, files in os.walk("./references_revised/"))
-mylist = []
 
-for f in cpt:
-	mylist.append(f)
+root_dir = './references_revised/'
 
-print len(mylist)
+dirnames = [] # collect all category directories in this list
+fnList = [] # collect all filenames in this list
+output = [] # collect all UNIQUE filenames in this list
+
+for refdirname, refdirnames, reffilenames in os.walk(root_dir):
+	dirnames.append(refdirname)
+
+dirnames = dirnames[1:] # remove root directory from directory list, only include subfolders (which are the categories)
+
+
+for dr in dirnames:
+	for rdname, rdnames, fns in os.walk(dr):
+		fnList.append(fns) # add all lists of filenames per category to the list
+
+fnList = [item for sublist in fnList for item in sublist] # flatten the list so we just have a list of all audio filenames
+
+print len(fnList)
+
+for x in fnList:
+    if x not in output:
+        output.append(x) # make a copy of the filename list containing only unique filenames
+
+print len(output) # number of unique named audio files
+
